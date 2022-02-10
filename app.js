a5f087afba5053834f4256883b597e60d38b09c7
atipattam <tamkjubu@gmail.com>
2022-02-11 00:40:43 +0700

    fix port

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('/api/v1/auth', authRouter)
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
