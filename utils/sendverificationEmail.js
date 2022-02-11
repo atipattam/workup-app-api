20fff1a8429ccd59e9c4ef7f4bf803ce4b6f4077
atipattam <tamkjubu@gmail.com>
2022-02-11 12:54:13 +0700

    fix path email

--- utils/sendverificationEmail.js
+++ utils/sendverificationEmail.js
@@ -2,7 +2,7 @@ const sendEmail = require('./sendEmail')
 
 const sendVerificationEmail = async({name,email,verificationToken,origin}) =>{
 
-  const verifyEmail = `${origin}/login/verify-email?token=${verificationToken}&email=${email}`
+  const verifyEmail = `${origin}/login/verify-account?token=${verificationToken}&email=${email}`
 
   const message = `<p>Please confirm your email by clicking on the following link :<a href="${verifyEmail}">Verify email</a> </p>`
   return sendEmail({to:email,
