da7373a26eda9bf8eb9d5db752f2d8bee448dca8
atipattam <tamkjubu@gmail.com>
2022-02-23 17:19:56 +0700

    test

--- app.js
+++ app.js
@@ -30,6 +30,19 @@ app.set('trust proxy', 1)
 //     resave: false,
 //   })
 // )
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Origin', req.headers.origin)
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,PUT,POST,DELETE,UPDATE,OPTIONS'
+  )
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+  )
+  next()
+})
 app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }))
 // credentials: true,
 // allowedHeaders: ['Content-Type', 'Authorization'],
