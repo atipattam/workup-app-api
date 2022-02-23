4dfd872f90a5f099fce8fabcf21c2124f3f278e5
atipattam <tamkjubu@gmail.com>
2022-02-23 16:37:50 +0700

    test

--- utils/jwt.js
+++ utils/jwt.js
@@ -15,17 +15,17 @@ const attachCookiesToResponse = ({ res, user, refreshToken }) => {
   const longerExp = 1000 * 60 * 60 * 24 * 30
   res.cookie('accessToken', accessTokenJWT, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    // secure: process.env.NODE_ENV === 'production',
     signed: true,
     expires: new Date(Date.now() + oneDay),
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   })
   res.cookie('refreshToken', refreshTokenJWT, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    // secure: process.env.NODE_ENV === 'production',
     signed: true,
     expires: new Date(Date.now() + longerExp),
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   })
 }
 
