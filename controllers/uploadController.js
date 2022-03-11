880fd6d0b7214bcefb6c545e8083aecda30c1882
atipattam <tamkjubu@gmail.com>
2022-03-11 11:17:29 +0700

    upload pdf

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,6 +1,6 @@
 const { StatusCodes } = require('http-status-codes')
-const path = require('path')
 const CustomError = require('../errors')
+const PDFDoc = require('pdfkit')
 const cloudinary = require('cloudinary').v2
 const fs = require('fs')
 
@@ -16,4 +16,16 @@ const uploadImage = async (req, res) => {
   return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } })
 }
 
-module.exports = { uploadImage }
+const uploadPDF = async (req,res) =>{
+   const result = await cloudinary.uploader.upload(
+    req.files.pdf.tempFilePath,
+    {
+      use_filename: true,
+      folder: 'workup-upload-pdf',
+    }
+  )
+  fs.unlinkSync(req.files.pdf.tempFilePath)
+  return res.status(StatusCodes.OK).json({ file: { src: result.secure_url } })
+}
+
+module.exports = { uploadImage, uploadPDF }
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const { uploadImage } = require('../controllers/uploadController')
+const { uploadImage, uploadPDF } = require('../controllers/uploadController')
 
 router.route('/uploads').post(uploadImage)
+router.route('/uploads/pdf').post(uploadPDF)
 
 module.exports = router
